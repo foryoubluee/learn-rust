2f2d7177759e4760e1945134427d854aa18bd56f
4ryasena <92186264+foryoubluee@users.noreply.github.com>
2022-11-10 00:41:25 +0700

    use sha256 crates

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,71 @@
-use std::vec;
+use rand::random;
+use sha256::digest;
 
 fn main() {
-	let mut i = 0;
-	let mut data_mahasiswa: Vec<&Mahasiswa> = vec![];
-
-	let sena = &Mahasiswa {
-		id: 1,
-		name: Name { fname: "Aryasena".to_owned(), lname: "Putra".to_owned() },
-		nilai: Nilai { nilai_tugas: 90.0, nilai_uas: 50.9 }
+	let mut new_user = &mut User {
+		name: "sena".to_owned(),
+		email: "awdawd@sawdaw.ks".to_owned(),
+		pocket: Pocket {
+			pocket_addr: random_pocket_address(),
+			balance: 0
+		}
+	};
+	let mut new_user2 = &mut User {
+		name: "bbb".to_owned(),
+		email: "dgdfgd@sawdaw.ks".to_owned(),
+		pocket: Pocket {
+			pocket_addr: random_pocket_address(),
+			balance: 0
+		}
 	};
 
-	while i < 5 {
-		data_mahasiswa.push(sena);
-		i+=1;
+	new_user.pocket.balance = 2;
+	new_user2.pocket.balance = 3;
+	println!("{:#?}", new_user);
+	println!("{:#?}", new_user2);
+	println!("{}", new_user.pocket.pocket_addr.len());
+	println!("{}", new_user2.pocket.pocket_addr.len());
+}
+
+#[derive(Debug)]
+struct Pocket {
+	pocket_addr: String,
+	balance: u128
+}
+
+#[derive(Debug)]
+struct User {
+	name: String,
+	email: String,
+	pocket: Pocket
+}
+
+trait Create {
+	fn new(&self) -> Self;
+}
+
+impl Create for User {
+	fn new(&self) -> Self {
+		User { name: self.name.to_owned(), email: self.email.to_owned(), pocket: Pocket { pocket_addr: self.pocket.pocket_addr.to_owned(), balance: self.pocket.balance } }
 	}
+}
 
-	for mhs in data_mahasiswa {
-		println!("{}", mhs.get_data());
+impl Create for Pocket {
+	fn new(&self) -> Self {
+		Pocket { pocket_addr: random_pocket_address(), balance: 0 }
 	}
 }
-impl Print<String> for Mahasiswa {
-	fn get_data(&self) -> String {
-			Mahasiswa {
-				id: self.id,
-				name: Name { fname: self.name.fname.to_owned(), lname: self.name.lname.to_owned() },
-				nilai: Nilai { nilai_tugas: self.nilai.nilai_tugas, nilai_uas: self.nilai.nilai_uas }
-			};
-			format!(
-				"{}. Name: {} {} | Nilai: {} {}", 
-				self.id,
-				self.name.fname.to_owned(),
-				self.name.lname.to_owned(),
-				self.nilai.nilai_tugas,
-				self.nilai.nilai_uas
-			)
-	}
-}
-trait Print<T> {
-	fn get_data(&self) -> T;
+
+fn random_pocket_address() -> String {
+	let mut used_addreses: Vec<String> = vec![];
+	let random_number = random::<u128>();
+	let hashed = hash(random_number.to_string());
+	used_addreses.push(hashed.to_owned());
+	println!("{:?}", used_addreses[0..1].to_owned());
+	format!("0x{}", hashed[2..64].to_owned())
 }
 
-struct Mahasiswa {
-	id: u8,
-	name: Name,
-	nilai: Nilai
-}
-
-struct Name {
-	fname: String,
-	lname: String
-}
-
-struct Nilai {
-	nilai_tugas: f32,
-	nilai_uas: f32
+fn hash(random: String) -> String {
+	let sha256 = digest(random.as_str());
+	sha256
 }
